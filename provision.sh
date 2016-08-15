fa246644ddb7d13997372c4ef4b7df7c63900d0f
Juergen Leopold <leopoldj@de.ibm.com>
2016-08-15 18:37:02 +0200

    First release.

---
+++ provision.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "Update Ubuntu ..."
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible -y
+apt-get update >/dev/null 2>&1
+apt-get -y upgrade >/dev/null 2>&1
+
+echo "Install Ansible ..."
+sudo apt-get -y install ansible
+
+ansible --version
+
+echo "Install pip ..."
+apt-get install -y python-pip
+apt-get install -y build-essential
+apt-get install -y python-dev
+
+echo "Install requests ..."
+pip install requests
+
+echo "Install virtualenv ..."
+pip install virtualenv
+
+echo "Install git ..."
+apt-get install -y git
