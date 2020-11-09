9dc552c231f34b4368ce747114f4c3515df6b2ed
Andreas Maier <maiera@de.ibm.com>
2020-11-10 00:34:25 +0100

    crypto_attach: Fixed an issue; Small optimization
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- zhmc_ansible_modules/zhmc_crypto_attachment.py
+++ zhmc_ansible_modules/zhmc_crypto_attachment.py
@@ -426,6 +426,8 @@ def ensure_attached(params, check_mode):
             raise Error("No crypto adapters of type {!r} found on CPC {!r} ".
                         format(crypto_type, cpc_name))
 
+        all_adapters_dict = {(a.name, a) for a in all_adapters}
+
         # All crypto adapters in a CPC have the same number of domains
         # (otherwise the concept of attaching domains across all attached
         # adapters cannot work). Therefore, the max number of domains can be
@@ -472,9 +474,8 @@ def ensure_attached(params, check_mode):
 
         # Verify the specified adapters exist
         if adapter_names is not None:
-            all_adapter_names = [a.name for a in all_adapters]
             for aname in adapter_names:
-                if aname not in all_adapter_names:
+                if aname not in all_adapters_dict:
                     raise ParameterError(
                         "The 'adapter_name' parameter specifies an adapter "
                         "named {!r} that does not exist in CPC {!r}".
@@ -734,6 +735,7 @@ def ensure_attached(params, check_mode):
             attached_adapter_names = [a.name for a in attached_adapters]
             for aname in adapter_names:
                 if aname not in attached_adapter_names:
+                    adapter = all_adapters_dict[aname]
 
                     # Check that the adapter has all needed domains available
                     conflicting_domains = get_conflicting_domains(
